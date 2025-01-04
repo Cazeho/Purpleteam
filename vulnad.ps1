518fb94ab74dcbb49dd39be03894e89776ba2be7
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-01-04 17:34:52 +0100

    Update vulnad.ps1

--- vulnad.ps1
+++ vulnad.ps1
@@ -1,3 +1,3 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 IEX((new-object net.webclient).downloadstring("https://raw.githubusercontent.com/wazehell/vulnerable-AD/master/vulnad.ps1"));
-Invoke-VulnAD -UsersLimit 100 -DomainName "DC01.local"
+Invoke-VulnAD -UsersLimit 100 -DomainName "lab.local"
