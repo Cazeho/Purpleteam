bd93f965ea3773c79815b2a2d08119d1608a6ef4
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-01-21 10:16:40 +0100

    Update splunk_config.ps1

--- forward/splunk_config.ps1
+++ forward/splunk_config.ps1
@@ -3,3 +3,8 @@ IEX((new-object net.webclient).downloadstring("https://raw.githubusercontent.com
 New-Item -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\win_outputs_app\local" -ItemType Directory -Force
 New-Item -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\powershell_inputs_app\local" -ItemType Directory -Force
 New-Item -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\sysmon_inputs_app\local" -ItemType Directory -Force
+New-Item -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\win_inputs_app\local" -ItemType Directory -Force
+
+
+
+
