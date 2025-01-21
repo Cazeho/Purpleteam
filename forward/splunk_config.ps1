2c0b7c6eface6c4963ed583eb83c3de3323c37a0
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-01-21 10:21:41 +0100

    Update splunk_config.ps1

--- forward/splunk_config.ps1
+++ forward/splunk_config.ps1
@@ -6,8 +6,8 @@ New-Item -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\sysmon_inputs
 New-Item -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\win_inputs_app\local" -ItemType Directory -Force
 
 
-C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" edit user admin -password Password123! -auth admin:Password123!
-C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" set default-hostname windows2019 -auth admin:Password123!
+"C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" edit user admin -password Password123! -auth admin:Password123!
+"C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" set default-hostname windows2019 -auth admin:Password123!
 
 
 sysmon64.exe -accepteula -i "C:\Program Files\sysmon\Sysmon.xml"
