3efed263af8cf69fe3278b6565fc6abbfac4c85c
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-01-21 10:11:21 +0100

    Update splunk_config.ps1

--- forward/splunk_config.ps1
+++ forward/splunk_config.ps1
@@ -1,4 +1,5 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+IEX((new-object net.webclient).downloadstring("https://raw.githubusercontent.com/Cazeho/Purpleteam/refs/heads/main/forward/splunk_config.ps1"));
 New-Item -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\win_outputs_app\local" -ItemType Directory -Force
 New-Item -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\powershell_inputs_app\local" -ItemType Directory -Force
 New-Item -Path "C:\Program Files\SplunkUniversalForwarder\etc\apps\sysmon_inputs_app\local" -ItemType Directory -Force
