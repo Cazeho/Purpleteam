ec9cf3406f613dae6c2f4bed9579cbaa6c0db45a
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-01-21 10:46:54 +0100

    Update splunk_config.ps1

--- forward/splunk_config.ps1
+++ forward/splunk_config.ps1
@@ -2,14 +2,14 @@
 IEX((new-object net.webclient).downloadstring("https://raw.githubusercontent.com/Cazeho/Purpleteam/refs/heads/main/forward/splunk_config.ps1"));
 
 
-"C:\Program Files\splunkforwarder-9.4.0-6b4ebe426ca6-windows-x64.msi" 'WINEVENTLOG_SEC_ENABLE=0 WINEVENTLOG_SYS_ENABLE=0 WINEVENTLOG_APP_ENABLE=0 PRIVILEGESECURITY=1 USE_LOCAL_SYSTEM=1 SPLUNKPASSWORD=Password123! AGREETOLICENSE=YES /quiet'
+& "C:\Program Files\splunkforwarder-9.4.0-6b4ebe426ca6-windows-x64.msi" 'WINEVENTLOG_SEC_ENABLE=0 WINEVENTLOG_SYS_ENABLE=0 WINEVENTLOG_APP_ENABLE=0 PRIVILEGESECURITY=1 USE_LOCAL_SYSTEM=1 SPLUNKPASSWORD=Password123! AGREETOLICENSE=YES /quiet'
 
-"C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe edit user admin -password Password123! -auth admin:Password123!"
-"C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe set default-hostname windows2019 -auth admin:Password123!"
+& "C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" edit user admin -password Password123! -auth admin:Password123!
+& "C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" set default-hostname windows2019 -auth admin:Password123!
 
 sysmon64.exe -accepteula -i "C:\Program Files\sysmon\Sysmon.xml"
 
-"C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe restart"
+& "C:\Program Files\SplunkUniversalForwarder\bin\splunk.exe" restart
 
 
 
