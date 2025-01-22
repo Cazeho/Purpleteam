bbe5d8e8e7ddbd839116f07702d1530353d2b79a
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-01-22 16:13:15 +0100

    Update install.sh

--- linux_forward/install.sh
+++ linux_forward/install.sh
@@ -1,1 +1,1 @@
-wget -O splunkforwarder-9.4.0-6b4ebe426ca6-linux-arm64.tgz "https://download.splunk.com/products/universalforwarder/releases/9.4.0/linux/splunkforwarder-9.4.0-6b4ebe426ca6-linux-arm64.tgz"
+wget -O splunkforwarder-9.4.0-6b4ebe426ca6-linux-amd64.tgz "https://download.splunk.com/products/universalforwarder/releases/9.4.0/linux/splunkforwarder-9.4.0-6b4ebe426ca6-linux-amd64.tgz"
